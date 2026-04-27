4b6818805a68cdffa746156c291352ef9fc15248
ManuelPauloAfonso <manuelpauloafonso29@gmail.com>
2026-04-27 02:22:48 +0100

    feat: ready for vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
+import tailwindcss from '@tailwindcss/vite';
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: 'static',
+  adapter: vercel(),
 });
